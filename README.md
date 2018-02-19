21d5d9a589aa10be7ba3c50079296ac9720bf994
Matthew D. Scholefield <matthew3311999@gmail.com>
2018-02-19 14:19:59 -0600

    General improvements  - Use difflib to perform fuzzy skill matching  - Mostly ignore insignificant words like 'skill' and 'fallback'  - Make uninstall work without 'Uninstall skill'. This is safer with Padatious because queries with words before 'Uninstall' like 'I need to uninstall music' won't activate

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-## Installer
-Skill installer interface
+# Installer Skill
 
-## Description 
-Tool for verbally installing and removing skills.  The general format is "install <skill identifier>", where <skill identifier> is the skill name or at least adequate subset of the name to uniquely identify the skill.  See the list of skill names at https://github.com/MycroftAI/mycroft-skills#community-contributed-skill-list.
-
-## Examples 
-* "install coin flip"
-* "uninstall skill coin flip"
-* "remove skill coin flip"
-
-## Credits 
-Mycroft AI, Inc.
+## Usage:
+* `install daily meditation`
--- __init__.py
+++ __init__.py
@@ -11,109 +11,180 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 import subprocess
-import re
-from os.path import join
+from difflib import SequenceMatcher
+from random import shuffle
+from subprocess import check_output
 
+import re
 from adapt.intent import IntentBuilder
+from os.path import join, isfile
+
 from mycroft import MYCROFT_ROOT_PATH
 from mycroft.configuration import ConfigurationManager
 from mycroft.skills.core import MycroftSkill, intent_handler
 
+__author__ = 'augustnmonteiro2'
 
 installer_config = ConfigurationManager.instance().get("SkillInstallerSkill")
 BIN = installer_config.get("path", join(MYCROFT_ROOT_PATH, 'msm', 'msm'))
 
 
 class SkillInstallerSkill(MycroftSkill):
+    SKILL_WORDS = ['scale']  # Words STT incorrectly transcribes "skill" to
+    COMMON_TOKENS = ['skill', 'fallback', 'mycroft']
+    ERROR_DIALOGS = {
+        5: 'system.error',  # missing.virtualenv
+        20: 'already.installed',
+        102: 'filesystem.error',  # could.not.access.directory
+        111: 'network.error',  # could.not.download.list
+        112: 'network.error',  # could.not.download.list
+        120: 'system.error',  # missing.virtualenv
+        121: 'installation.error',  # python.dependency.installation.failed
+        122: 'installation.error',  # dependency.installation.failed
+        123: 'filesystem.error',  # could.not.change.permissions
+        202: 'not.found',  # skill.not.in.repo
+        252: 'not.found',  # skill.not.in.repo
+        249: 'filesystem.error',  # remove.permission.denied
+        253: 'remove.not.found'
+    }
+
     def __init__(self):
         super(SkillInstallerSkill, self).__init__(name="SkillInstallerSkill")
+        self.ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+
+    def initialize(self):
+        try:
+            self.register_intent_file('uninstall.intent', self.uninstall)
+            self.register_intent_file('list.skills.intent', self.handle_list_skills)
+        except AttributeError:
+            self.log.exception('Running outdated version')
+
+    def __translate_list(self, list_name):
+        try:
+            return self.translate_list(list_name)
+        except AttributeError:
+            list_file = join(self.vocab_dir, 'action.list')
+            if isfile(list_file):
+                with open(list_file) as f:
+                    return f.read().split('\n')
+            self.log.error('Missing list: ' + list_file)
+            return ['<invalid>']
+
+    def get_skill_list(self):
+        """Finds the names of all installed skills"""
+        return [
+            i.strip() for i in
+            self.ansi_escape.sub('', check_output([BIN, 'list'])).split('\n')
+        ]
+
+    def search_for_skill(self, search, skill_list):
+        """Returns list of possible skills"""
+
+        def extract_tokens(s, tokens):
+            extracted = []
+            for token in tokens:
+                extracted += token * s.count(token)
+                s = s.replace(token, '')
+            s = ' '.join(i for i in s.split(' ') if i)
+            return s, extracted
+
+        def compare_seq(a, b):
+            return SequenceMatcher(a=a, b=b).ratio()
+
+        for close_word in self.SKILL_WORDS:
+            search = search.replace(close_word, 'skill')
+        search, search_common = extract_tokens(search, self.COMMON_TOKENS)
+        search_tokens = [i for i in search.split(' ') if i]
+
+        confidences = {}
+        for skill in skill_list:
+            skill = skill.replace('[installed]', '').strip()
+            if not skill:
+                continue
+            full_name = skill
+            skill = skill.replace('-', ' ')
+            skill, skill_common = extract_tokens(skill, self.COMMON_TOKENS)
+
+            char_conf = compare_seq(skill, search)
+            word_conf = compare_seq(skill.split(' '), search_tokens)
+            common_conf = compare_seq(skill_common, search_common)
+            conf = (0.45 * char_conf + 0.45 * word_conf + 0.1 * common_conf)
+
+            confidences[full_name] = conf
+
+        best_skill, best_conf = max(confidences.items(), key=lambda x: x[1])
+        best_skills = [s for s, c in confidences.items() if c > best_conf - 0.1]
+
+        self.log.info('Highest Confidence Skill: ' + best_skill)
+        self.log.info('Highest Confidence: ' + str(best_conf))
+
+        if best_conf < 0.4:
+            return []
+        elif best_conf == 1.0:
+            return [best_skill]
+        else:
+            return best_skills
 
     @intent_handler(IntentBuilder("InstallIntent").require("Install"))
     def install(self, message):
-        utterance = message.data.get('utterance').lower()
-        name = utterance.replace(message.data.get('Install'), '')
+        action = self.__translate_list('action')[0]
         self.speak_dialog("installing")
+        utterance = message.data['utterance'].lower()
+        search = utterance.replace(message.data['Install'], '').strip()
+        skills = self.search_for_skill(search, self.get_skill_list())
 
-        # Invoke MSM to perform installation
-        try:
-            cmd = ' '.join([BIN, 'install', '"' + name.strip() + '"'])
-            output = subprocess.check_output(cmd, shell=True)
-            self.log.info("MSM output: " + str(output))
-            rc = 0
-        except subprocess.CalledProcessError, e:
-            output = e.output
-            rc = e.returncode
-
-        if rc == 0:
-            # Success!
-            self.speak_dialog("installed", data={'skill': name})
-        elif rc == 20:
-            # Already installed
-            self.speak_dialog("already.installed", data={'skill': name})
-        elif rc == 201:
-            # Multiple matches found
-
-            # A line of dashes starts and ends the list of skills in output
-            pat = re.compile("----------$(.*)^----------",
-                             re.DOTALL | re.MULTILINE)
-            match = pat.search(output)
-            if match:
-                skills = match.group(1)
+        if not skills:
+            self.speak_dialog("not.found", dict(skill=search, action=action))
+        elif len(skills) == 1:
+            skill = skills[0]
+            try:
+                output = check_output([BIN, 'install', skill])
+            except subprocess.CalledProcessError as e:
+                self.log.error("MSM returned " + str(e.returncode) + ": " + e.output)
+                dialog = self.ERROR_DIALOGS.get(e.returncode, "installation.error")
+                self.speak_dialog(dialog, dict(skill=skill, action=action,
+                                               error=e.returncode))
             else:
-                skills = ""
-
-            # read the list for followup
-            self.speak_dialog("choose", data={'skills': ", ".join(skills)})
-        elif rc == 202:
-            # Not found
-            self.speak_dialog("not.found", data={'skill': name})
+                self.log.info("MSM output: " + str(output))
+                self.speak_dialog("installed", data={'skill': skill})
         else:
-            # Other installation error, just read code
-            self.speak_dialog("installation.error", data={'skill': name,
-                                                          'error': rc})
+            if len(skills) < 8:
+                self.speak_dialog("choose", data={'skills': ", ".join(skills)})
+            else:
+                self.speak_dialog('too.many.matches')
 
-    @intent_handler(IntentBuilder("UninstallIntent").require("Uninstall"))
+    def handle_list_skills(self, message):
+        skill_list = self.get_skill_list()
+        shuffle(skill_list)
+        skills = ', '.join(skill_list[:4])
+        self.speak_dialog('some.available.skills', dict(skills=skills))
+
     def uninstall(self, message):
-        utterance = message.data.get('utterance').lower()
-        name = utterance.replace(message.data.get('Uninstall'), '')
+        if 'skill' not in message.data:
+            self.speak_dialog('remove.not.found')
+            return
+
+        action = self.__translate_list('action')[1]
         self.speak_dialog("removing")
+        search = message.data['skill']
+        skills = self.search_for_skill(search, self.get_skill_list())
 
-        # Invoke MSM to perform removal
-        try:
-            cmd = ' '.join([BIN, 'remove', '"' + name.strip() + '"'])
-            output = subprocess.check_output(cmd, shell=True)
-            self.log.info("MSM output: " + str(output))
-            rc = 0
-        except subprocess.CalledProcessError, e:
-            output = e.output
-            rc = e.returncode
-
-        if rc == 0:
-            # Success, removed!
-            self.speak_dialog("removed", data={'skill': name})
-        elif rc == 253:
-            # Already installed
-            self.speak_dialog("remove.not.found", data={'skill': name})
-        elif rc == 251:
-            # Multiple matches found
-
-            # A line of dashes starts and ends the list of skills in output
-            pat = re.compile("----------$(.*)^----------",
-                             re.DOTALL | re.MULTILINE)
-            match = pat.search(output)
-            if match:
-                skills = match.group(1)
+        if not skills:
+            self.speak_dialog("remove.not.found", dict(skill=search))
+        elif len(skills) == 1:
+            skill = skills[0]
+            # Invoke MSM to perform removal
+            try:
+                output = check_output([BIN, 'remove', skill])
+            except subprocess.CalledProcessError as e:
+                self.log.error("MSM returned " + str(e.returncode) + ": " + e.output)
+                dialog = self.ERROR_DIALOGS.get(e.returncode, "removal.error")
+                self.speak_dialog(dialog, data=dict(skill=skill, action=action,
+                                                    error=e.returncode))
             else:
-                skills = ""
-
-            # read the list for followup
-            self.speak_dialog("choose", data={'skills': ", ".join(skills)})
-        else:
-            # Other removal error, just read code
-            self.speak_dialog("removal.error", data={'skill': name,
-                                                     'error': rc})
+                self.log.info("MSM output: " + str(output))
+                self.speak_dialog("removed", data={'skill': skill})
 
 
 def create_skill():
